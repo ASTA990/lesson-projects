285584844606c627f705ad8761fa1d3c4114e377
ASTA990 <grandsaidjon@gmail.com>
2026-04-29 17:02:37 +0500

    add

---
+++ javascript.asoslari/main.js
@@ -0,0 +1,11 @@
+let birinchiSon = 3
+let ikkinchiSon = 4 
+let uchinchiSon = 899
+
+let ayirish = birinchiSon - ikkinchiSon
+let qoshish = uchinchiSon + birinchiSon
+let hamma_yigindi = birinchiSon+ikkinchiSon+uchinchiSon
+
+console.log("Ayirish", ayirish)
+console.log("Qoshish", qoshish)
+console.log("Hammasi", hamma_yigindi)
